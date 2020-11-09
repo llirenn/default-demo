ac4d73477b2231d09ed0c8e4f960026dcc401afc
llirenn <18707192877@163.com>
2020-11-09 20:23:23 +0800

    element-vue

---
+++ src/views/Home.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="home">
+    <el-row>
+      <el-button>默认按钮</el-button>
+      <el-button type="primary">主要按钮</el-button>
+      <el-button type="success">成功按钮</el-button>
+      <el-button type="info">信息按钮</el-button>
+      <el-button type="warning">警告按钮</el-button>
+      <el-button type="danger">危险按钮</el-button>
+    </el-row> 
+<el-input v-model="input" placeholder="请输入内容"></el-input>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  // components: {
+  //   HelloWorld
+  // },
+  data(){
+     return {
+      input: ''
+    }
+  },
+
+  mounted(){
+    this.$axios.get('baidu.com').then(res=>{
+      
+    })
+  }
+}
+</script>
